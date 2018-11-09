2000eaeaf4cf7a0bd69e7e4d75a671ce2b00a30d
Dean D. George <Dean.D.George@students.ittralee.ie>
2018-11-09 13:00:49 +0000

    Signed-off-by: Dean D. George <Dean.D.George@students.ittralee.ie>

---
+++ Worms 3D/Assets/Loot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour {
+
+    public class DropCurrency
+    {
+        public string name;
+        public GameObject Item;
+        public int dropChance;
+    }
+
+    public List<DropCurrency> LootTable = new List<DropCurrency> { };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
